73519763cd0c711c72ba5b3c6f379339a33553ec
kaczla <kaczla@wp.pl>
2015-11-24 21:49:21 +0100

    Update
    Fix formating.

--- Client/src/main/java/tasslegro/MyUI.java
+++ Client/src/main/java/tasslegro/MyUI.java
@@ -1,7 +1,5 @@
 package tasslegro;
 
-import java.io.File;
-
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
@@ -9,30 +7,18 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
-
-import com.vaadin.server.FileResource;
-import com.vaadin.server.FontAwesome;
-import com.vaadin.server.VaadinService;
-import com.vaadin.ui.AbsoluteLayout;
-import com.vaadin.ui.Image;
-import com.vaadin.ui.PasswordField;
-import com.vaadin.ui.TextField;
 
 @Theme("mytheme")
 @Widgetset("tasslegro.MyAppWidgetset")
 public class MyUI extends UI {
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-    }
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+	}
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
 }
